ea193fc5555eccd1137e6709d538d985c0a9f831
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2024-11-17 21:32:16 +0100

    Second lesson finished

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
-
+  //Para que las variables sean accesibles desde el front deben ser públicas (por defecto lo son)
+  welcome = 'Bienvenido';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear proyecto',
+    'Crear componente',
+    'Crear servicio'
+  ];
+  disabled = true;
+  person = {
+    name: "Sergio",
+    age: 22,
+    avatar: "https://fastly.picsum.photos/id/903/200/200.jpg?hmac=lxHKyjlQqAkKyuVGkgUCO_jdWkg3osj3nTuULFHZxH8"
+  }
 }
